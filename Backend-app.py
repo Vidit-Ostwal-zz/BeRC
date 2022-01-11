7904becaf376b972295b77485f1b78a3290ae8ed
Vidit <viditostwal@gmail.com>
2022-01-11 14:39:43 +0530

    Add files via upload

---
+++ Backend-app.py
@@ -0,0 +1,23 @@
+from jina import DocumentArray, Flow
+from jina.types.document.generators import from_files
+
+def check_query(resp):
+    for d in resp.docs:
+        print(f'{d.uri}, {len(d.chunks)}')
+        for m in d.matches:
+            print(f'+- {m.uri}: {m.scores["cosine"].value:.6f}, {m.tags}')
+
+def main():
+    docs = DocumentArray(from_files('data/*.wav'))
+
+    f = Flow.load_config('flow.yml')
+    with f:
+        f.post(on='/index', inputs=docs)
+        f.post(on='/search', inputs=docs, on_done=check_query)
+        f.protocol = 'http'
+        f.cors = True
+        f.block()
+
+
+if __name__ == '__main__':
+    main()
